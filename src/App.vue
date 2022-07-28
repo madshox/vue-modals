263c33ce8fad38e1e145137bc83b22857409b454
madshox <63535913+madshox@users.noreply.github.com>
2022-07-28 10:49:38 +0500

    feat: modal body, close animation

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,21 @@
               Show first modal
             </button>
             <!-- first modal -->
-            <modals title="First modal" v-if="modalFirst"></modals>
+            <modals
+              title="First modal"
+              v-show="modalFirst"
+              @close="modalFirst = !modalFirst"
+            >
+              <div slot="body">
+                <p>Lorem ipsum dolor sit amet</p>
+                <button
+                  class="btn btnPrimary"
+                  @click="modalFirst = !modalFirst"
+                >
+                  Well done!
+                </button>
+              </div>
+            </modals>
           </div>
         </section>
       </div>
