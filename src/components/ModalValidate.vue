f6b8c390d30608e8bd7f1d66990c2f749b7c7809
madshox <63535913+madshox@users.noreply.github.com>
2022-07-28 14:03:25 +0500

    feat: form validation email, name fields

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -1,14 +1,35 @@
 <template>
-  <modal
-    title="Modal with form + validate"
-    @close="$emit('close')"
-  >
+  <modal title="Modal with form + validate" @close="$emit('close')">
     <div slot="body">
       <form @submit.prevent="submitSecondForm">
-        <label>Name:</label>
-        <input type="text" />
-        <label>Email:</label>
-        <input type="email" />
+      
+        <!-- name -->
+        <div class="form-item" :class="{ errorInput: $v.name.$error }">
+          <label>Name:</label>
+          <p class="errorText" v-if="!$v.name.required">Field is required!</p>
+          <p class="errorText" v-if="!$v.name.minLength">
+            Name must have at least {{ $v.name.$params.minLength.min }}!
+          </p>
+          <input
+            v-model="name"
+            :class="{ error: $v.name.$error }"
+            @change="$v.name.$touch()"
+          />
+        </div>
+
+        <!-- email -->
+        <div class="form-item" :class="{ errorInput: $v.email.$error }">
+          <label>Email:</label>
+          <p class="errorText" v-if="!$v.email.required">Field is required!</p>
+          <p class="errorText" v-if="!$v.email.email">Email is not correct!</p>
+          <input
+            v-model="email"
+            :class="{ error: $v.email.$error }"
+            @change="$v.email.$touch()"
+          />
+        </div>
+
+        <!-- button -->
         <button class="btn btnPrimary">Submit!</button>
       </form>
     </div>
@@ -16,8 +37,44 @@
 </template>
 
 <script>
+import { required, minLength, email } from "vuelidate/lib/validators";
+
 import modal from "@/components/UI/Modal.vue";
+
 export default {
   components: { modal },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4),
+    },
+    email: {
+      required,
+      email,
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.form-item .errorText {
+  display: none;
+  margin-bottom: 8px;
+  font-size: 13.4px;
+  color: #be1e1e;
+}
+.form-item {
+  &.errorInput .errorText {
+    display: block;
+  }
+}
+input.error {
+  border-color: #be1e1e;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
+
+// Plugins
+Vue.use(Vuelidate)
 
 import './assets/scss/main.scss'
 
